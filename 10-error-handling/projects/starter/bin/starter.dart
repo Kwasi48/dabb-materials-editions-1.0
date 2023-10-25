231be5fb2362a6aa03af878b29fb001d23916d83
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-10-25 06:03:21 +0000

    handling Specific errors

--- 10-error-handling/projects/starter/bin/starter.dart
+++ 10-error-handling/projects/starter/bin/starter.dart
@@ -1,6 +1,63 @@
 import 'dart:convert';
 
 void main() {
-  const malformedJson = 'abc';
-  jsonDecode(malformedJson);
+  const numberStrings = ["42", "hello"];
+
+  try {
+    for (final numberString in numberStrings) {
+      final number = int.parse(numberString);
+      print(number ~/ 0);
+    }
+  } on FormatException {
+    handleFormatException();
+  } on UnsupportedError {
+    handleDivisionByZero();
+  }
 }
+
+void handleFormatException() {
+  print('You tried to parse a non-numeric string.');
+}
+
+void handleDivisionByZero() {
+  print("You can't divide by zero.");
+}
+
+
+
+
+
+ // final characters = ' abcdefghijklmnopqrstuvwxyz!';
+  // final data = [4, 1, 18, 20, 0, 9, 19, 0, 6, 21, 14, 27];
+  // final buffer = StringBuffer();
+  // for (final index in data) {
+  //   final letter = characters[index];
+  //   buffer.write(letter);
+  // }
+  // print(buffer);
+
+// //   const malformedJson = 'abc';
+// //   jsonDecode(malformedJson);
+
+//   functionOne();
+// }
+
+// void functionOne() {
+//   functionTwo();
+// }
+
+// void functionTwo() {
+//   functionThree();
+// }
+
+// void functionThree() {
+//   int.parse('Hello');
+
+// const json = ' abc';
+
+//   try {
+//     dynamic result = jsonDecode(json);
+//     print(result);
+//   } on FormatException {
+//     print('The JSON string was invalid');
+//   }
