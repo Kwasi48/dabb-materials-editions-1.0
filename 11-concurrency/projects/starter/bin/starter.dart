b5759ccbe1d486c1c19f7df197d5db718f8b8e6a
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-10-26 09:41:36 +0000

    adding a task tothe microtask queue

--- 11-concurrency/projects/starter/bin/starter.dart
+++ 11-concurrency/projects/starter/bin/starter.dart
@@ -1,3 +1,21 @@
-void main() {
-  
-}
+void main() {}
+
+  // print('first');
+  //adding a task to the event queue.
+  // Future(
+  //   () => print('second'),
+  // );
+
+  // print('third');
+
+
+
+   // print('first');
+
+  // Future(
+  //   () => print('second'),
+  // );
+  // //adding a task to the microtask queue
+  // Future.microtask(() => print('third'));
+
+  // print('four');
