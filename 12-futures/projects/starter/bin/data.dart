2b3270c4c09d4ca297d29a18552b2b5eefe990be
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-10-27 06:07:24 +0000

    working with http

---
+++ 12-futures/projects/starter/bin/data.dart
@@ -0,0 +1,29 @@
+class Todo {
+  Todo({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.completed,
+  });
+
+  factory Todo.fromJson(Map<String, dynamic> jsonMap) {
+    return Todo(
+        userId: jsonMap['userId'] as int,
+        id: jsonMap['id'] as int,
+        title: jsonMap['title'] as String,
+        completed: jsonMap['completed'] as bool);
+  }
+
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
+
+  @override
+  String toString() {
+    return 'userId: $userId\n'
+        'id: $id'
+        'title: $title'
+        'completed: $completed';
+  }
+}
