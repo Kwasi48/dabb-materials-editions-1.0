674f51895dee8d649708701b3000c297fc866f92
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-10-27 05:48:04 +0000

    using async and await

--- 12-futures/projects/starter/bin/starter.dart
+++ 12-futures/projects/starter/bin/starter.dart
@@ -1,10 +1,17 @@
-void main() {
+Future<void> main() async {
   print('Before the future');
 
-  final myFuture = Future<int>.delayed(Duration(seconds: 1), (() => 42))
-      .then((value) => print('Value: $value'))
-      .catchError((Object error) => print('Error: $error'))
-      .whenComplete(() => print('Future is complete'));
-
+  final value = await Future<int>.delayed(Duration(seconds: 1), (() => 42));
+  print('Value: $value');
   print('After the Future');
 }
+
+
+//  print('Before the future');
+
+//   final myFuture = Future<int>.delayed(Duration(seconds: 1), (() => 42))
+//       .then((value) => print('Value: $value'))
+//       .catchError((Object error) => print('Error: $error'))
+//       .whenComplete(() => print('Future is complete'));
+
+//   print('After the Future');
