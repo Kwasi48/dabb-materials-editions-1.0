9210de9ff17e05e3d22323a40725f8a4a2d55119
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-10-30 19:52:50 +0000

    trying to instantiate the fakewebserver class

--- 12-futures/projects/starter/bin/sctatch.dart
+++ 12-futures/projects/starter/bin/sctatch.dart
@@ -4,7 +4,7 @@ abstract class DataRepository {
 
 class FakeWebServer implements DataRepository {
   @override
-  Future<double> fetchTemperature(String city) {
+  Future<double> fetchTemperature(String city) async {
     //return Future(() => 42);\
     //completing with a value
     //return Future.value(42);
@@ -12,6 +12,11 @@ class FakeWebServer implements DataRepository {
     //return Future.error(ArgumentError("$city doesn't exist"));
     //Giving a Delayed Response with error
     return Future.delayed(Duration(seconds: 2),
-        (() => throw ArgumentError('City does not exis')));
+        (() => throw ArgumentError('City does not exist')));
   }
+}
+
+Future<void> main() async {
+  final server = FakeWebServer();
+  print(server.fetchTemperature('havanna'));
 }
