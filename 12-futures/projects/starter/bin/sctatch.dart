c9f6dd697e081594fa98e30b3b562c8a2e38270d
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-10-30 19:41:38 +0000

    completing with an Error

--- 12-futures/projects/starter/bin/sctatch.dart
+++ 12-futures/projects/starter/bin/sctatch.dart
@@ -5,7 +5,10 @@ abstract class DataRepository {
 class FakeWebServer implements DataRepository {
   @override
   Future<double> fetchTemperature(String city) {
-    //return Future(() => 42);
-    return Future.value(42);
+    //return Future(() => 42);\
+    //completing with a value
+    //return Future.value(42);
+    //completing with an error
+    return Future.error(ArgumentError("$city doesn't exist"));
   }
 }
