1a0090bda4a918d91076af7dd905fd9e6a67281d
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-10-31 08:50:09 +0000

    challenge 1

--- 12-futures/projects/starter/bin/challenges.dart
+++ 12-futures/projects/starter/bin/challenges.dart
@@ -1,12 +1,33 @@
 //spotty Internet
 import 'dart:async';
+import 'dart:io';
+import 'dart:math';
 
 abstract class DataRepository {
   Future<double> fetchTemperature(String city);
 }
 
+Future<void> checkTemperature(DataRepository repo) async {
+  try {
+    final city = 'Portland';
+    final degrees = await repo.fetchTemperature(city);
+    print("It's $degrees degress in $city");
+  } on HttpException catch (e) {
+    print(e);
+  }
+}
+
 class FakeWebServer implements DataRepository {
   //using the completer class
   @override
-  Future<double> fetchTemperature(String city) {}
+  Future<double> fetchTemperature(String city) {
+    var random = Random();
+    final errorvalue = random.nextBool();
+    if (errorvalue) {
+      return Future.error(HttpException("couldn't reach the server"));
+    } else {
+      final temp = random.nextDouble() * 50;
+      return Future.value(temp);
+    }
+  }
 }
