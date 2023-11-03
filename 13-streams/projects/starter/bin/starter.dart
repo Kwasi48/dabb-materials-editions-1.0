a86cc87aa531cda8f0a4c819e082681841641d1d
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-11-03 03:11:08 +0000

    reading from a file using dart:io

--- 13-streams/projects/starter/bin/starter.dart
+++ 13-streams/projects/starter/bin/starter.dart
@@ -1,3 +1,8 @@
-void main() {
-  
+import 'dart:io';
+
+Future<void> main() async {
+  final file = File(
+      'C:/Users/micha/OneDrive/Desktop/books/dabb-materials-editions-1.0/13-streams/projects/starter/assets/text.txt');
+  final contents = await file.readAsString();
+  print(contents);
 }
