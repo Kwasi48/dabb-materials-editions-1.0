5d1aeb5e9f7c249cd73d874c5441028037b5402f
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-11-03 03:46:23 +0000

    Cancelling a Stream

--- 13-streams/projects/starter/bin/starter.dart
+++ 13-streams/projects/starter/bin/starter.dart
@@ -1,20 +1,24 @@
 import 'dart:io';
+import 'dart:async';
 
 Future<void> main() async {
-  //Using try catch to handle errors
-  try {
-    final file = File(
-        'C:/Users/micha/OneDrive/Desktop/books/dabb-materials-editions-1.0/13-streams/projects/starter/assets/text_long.txt');
-    final stream = file.openRead();
-    await for (var data in stream) {
+  //Cancelling a Stream
+
+  final file = File(
+      'C:/Users/micha/OneDrive/Desktop/books/dabb-materials-editions-1.0/13-streams/projects/starter/assets/text_long.txt');
+  final stream = file.openRead();
+  StreamSubscription<List<int>>? subscription;
+  subscription = stream.listen(
+    (data) {
       print(data.length);
-    }
-  } on Exception catch (e) {
-    print(e);
-  } finally {
-    print("all finished");
-  }
-}
+      subscription?.cancel();
+    },
+    cancelOnError: true,
+    onDone: () {
+      print('all finished');
+    },
+  );
+}  
 
 
 //reading text from a single file 
