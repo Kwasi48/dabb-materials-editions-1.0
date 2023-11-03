97ab3d06bfdb9fe22683391a36cd7b3824469609
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-11-03 03:26:08 +0000

    reading from  a stream

--- 13-streams/projects/starter/bin/starter.dart
+++ 13-streams/projects/starter/bin/starter.dart
@@ -1,8 +1,18 @@
 import 'dart:io';
 
 Future<void> main() async {
+  //reading from a stream
   final file = File(
-      'C:/Users/micha/OneDrive/Desktop/books/dabb-materials-editions-1.0/13-streams/projects/starter/assets/text.txt');
-  final contents = await file.readAsString();
-  print(contents);
+      'C:/Users/micha/OneDrive/Desktop/books/dabb-materials-editions-1.0/13-streams/projects/starter/assets/text_long.txt');
+  final stream = file.openRead();
+  stream.listen((data) {
+    print(data.length);
+  });
 }
+
+
+//reading text from a single file 
+  // final file = File(
+  //     'C:/Users/micha/OneDrive/Desktop/books/dabb-materials-editions-1.0/13-streams/projects/starter/assets/text.txt');
+  // final contents = await file.readAsString();
+  // print(contents);
