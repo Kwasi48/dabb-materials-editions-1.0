967c5ea1ccfe55b526d290246ee73c3a22c2b46d
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-11-05 06:04:27 +0000

    added  a package

---
+++ 13-streams/projects/starter/bin/challenges.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<void> main() async {
+  final url = Uri.parse('https://kodeco.com');
+  final client = http.Client();
+  final request = http.Request('GET', url);
+  final response = await client.send(request);
+  final stream = response;
+}
