8893fc1f76788d4c1ea2ab85af3d8e4223736918
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-11-05 12:25:31 +0000

    solution updated

--- 13-streams/projects/starter/bin/challenges.dart
+++ 13-streams/projects/starter/bin/challenges.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:http/http.dart' as http;
 
 Future<void> main() async {
+  //challenge 1
   final url = Uri.parse('https://kodeco.com');
   final client = http.Client();
   final request = http.Request('GET', url);
@@ -13,8 +13,14 @@ Future<void> main() async {
   //   print(value);
   // });
 
-  final contents = stream.transform(utf8.decoder);
-  await for (var con in contents) {
-    print(con);
+  try {
+    final contents = stream.transform(utf8.decoder);
+    await for (var con in contents) {
+      print(con.length);
+    }
+  } on Exception catch (e) {
+    print(e);
+  } finally {
+    client.close();
   }
 }
