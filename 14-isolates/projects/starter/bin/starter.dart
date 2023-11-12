9c162352f7683fbd2ae549f44289fc858c9d0d11
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-11-12 22:35:48 +0000

    sending multple messages

--- 14-isolates/projects/starter/bin/starter.dart
+++ 14-isolates/projects/starter/bin/starter.dart
@@ -1,28 +1,36 @@
 import 'dart:isolate';
 
-void playHideandSeekTheLongVersion(SendPort sendPort) {
+void playHideandSeekTheLongVersion(List<Object> arguments) {
+  final sendPort = arguments[0] as SendPort;
+  final countTo = arguments[1] as int;
+
+  sendPort.send("OK, I'am counting.......");
+
   var counting = 0;
-  for (var i = 1; i <= 1000000000; i++) {
+  for (var i = 1; i <= countTo; i++) {
     counting = i;
   }
-  final message = '$counting! Ready or not, here I come';
-  Isolate.exit(sendPort, message);
+
+  sendPort.send('$counting! Ready or not, here I come');
+  sendPort.send(null);
 }
 
 Future<void> main() async {
   //1
   final receivePort = ReceivePort();
 
-  //2
-  await Isolate.spawn<SendPort>(
-    //3
-    playHideandSeekTheLongVersion,
-
-    //4
-    receivePort.sendPort,
-  );
-
-  //5
-  final message = await receivePort.first as String;
-  print(message);
+  final isolate = await Isolate.spawn((message) { }, message)
 }
+
+//   //2
+//   await Isolate.spawn<SendPort>(
+//     //3
+//     playHideandSeekTheLongVersion,
+
+//     //4
+//     receivePort.sendPort,
+//   );
+
+//   //5
+//   final message = await receivePort.first as String;
+//   print(message);
