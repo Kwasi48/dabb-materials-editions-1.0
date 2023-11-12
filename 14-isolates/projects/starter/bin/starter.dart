f1f16ca3223ad59752104258bbe86d38d502ffc5
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-11-12 21:55:05 +0000

    isolates

--- 14-isolates/projects/starter/bin/starter.dart
+++ 14-isolates/projects/starter/bin/starter.dart
@@ -1,3 +1,28 @@
-void main() {
-  
+import 'dart:isolate';
+
+void playHideandSeekTheLongVersion(SendPort sendPort) {
+  var counting = 0;
+  for (var i = 1; i <= 1000000000; i++) {
+    counting = i;
+  }
+  final message = '$counting! Ready or not, here I come';
+  Isolate.exit(sendPort, message);
+}
+
+Future<void> main() async {
+  //1
+  final receivePort = ReceivePort();
+
+  //2
+  await Isolate.spawn<SendPort>(
+    //3
+    playHideandSeekTheLongVersion,
+
+    //4
+    receivePort.sendPort,
+  );
+
+  //5
+  final message = await receivePort.first as String;
+  print(message);
 }
