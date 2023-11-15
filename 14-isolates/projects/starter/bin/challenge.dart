2dd8824795c28f5bf01d0c66c506881628423f19
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-11-15 11:51:26 +0000

    challenge 1 solved

--- 14-isolates/projects/starter/bin/challenge.dart
+++ 14-isolates/projects/starter/bin/challenge.dart
@@ -1,7 +1,15 @@
 //challenge 1
 import 'dart:isolate';
 
-Future<void> main() async {}
+Future<void> main() async {
+  final receivePort = ReceivePort();
+  int n = 10;
+  final arguments = {'n': n, 'sendPort': receivePort.sendPort};
+
+  await Isolate.spawn(fibonacci, arguments);
+  final message = await receivePort.first as int;
+  print('the fibonacci number of $n is $message');
+}
 
 void fibonacci(Map<String, Object> arguments) {
   final sendPort = arguments['sendPort'] as SendPort;
