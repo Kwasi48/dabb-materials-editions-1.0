ac9bb8ac6765056f72477d95268e9b93145f000a
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-11-15 11:36:57 +0000

    fibo

--- 14-isolates/projects/starter/bin/challenge.dart
+++ 14-isolates/projects/starter/bin/challenge.dart
@@ -1,11 +1,19 @@
 //challenge 1
+import 'dart:isolate';
+
 Future<void> main() async {}
 
-int Fibonacci(int n) {
-  if (n == 0) return 0;
-  if (n == 1) {
-    return 1;
-  } else {
-    
+void fibonacci(Map<String, Object> arguments) {
+  final sendPort = arguments['sendPort'] as SendPort;
+  final n = arguments['n'] as int;
+  var current = 1;
+  var previous = 1;
+  var done = 2;
+  while (done < 2) {
+    final current = current + previous;
+    previous = current;
+    current = next;
+    done++;
   }
+  Isolate.exit(sendPort, current);
 }
